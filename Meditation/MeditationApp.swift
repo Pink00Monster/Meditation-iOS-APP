237e5403c9cb333d92b910e8d378eabd8b34aaf2
Pink00Monster <vanillaWangyi@163.com>
2023-04-29 10:38:39 +0200

    Create the project

---
+++ Meditation/MeditationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditationApp.swift
+//  Meditation
+//
+//  Created by Vanilla on 29/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct MeditationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
